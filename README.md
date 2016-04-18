ad9e8fda417895b6452bf5875985799e267fbdab
Hauke Stieler <mail@hauke-stieler.de>
2016-04-18 02:53:43 +0200

    Habe die Abgabe für Blatt 1 und das download-script für die packages dadp und gail hinzugefügt, sowie die README angepasst.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Hausaufgaben
 Unsere Hausaufgaben im Modul `Lösungsstrategien für NP-schwere Probleme der
 Kombinatorischen Optimierung` an der Uni Hamburg.
+
+# Rulez
+Bitte führt jedes mal, wenn ihr irgendwie `git pull` macht auch eben die `getpackages.sh` aus. Wenn ihr windows benutzt meckert nicht, wenn LaTeX fehler ausspuckt, weil irgendwas in den packages `dadp` oder `gail` kaputt ist ;)
---
+++ getpackages.sh
@@ -0,0 +1,7 @@
+echo "DOWNLOADING NEWEST PACKAGES...\n\n"
+echo "	GET dadp...\n\n"
+wget https://github.com/hauke96/LaTeX/blob/master/dadp.sty
+echo "\n	GET gail...\n\n"
+wget https://github.com/hauke96/LaTeX/blob/master/gail.sty
+echo "\n\n	DONE\n	(does not mean that everything just worked fine :P )\n"
+echo "	You can also use \"git clone https://github.com/hauke96/LaTeX.git\" get get everything.\n\n"
