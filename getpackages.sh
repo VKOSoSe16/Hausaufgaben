de4138185950a34ba299b43cd2572e54b1b54f5c
Hauke Stieler <mail@hauke-stieler.de>
2016-05-01 17:26:20 +0200

    gail.sty und getpackage script aktualisiert.

--- getpackages.sh
+++ getpackages.sh
@@ -1,7 +1,9 @@
-echo "DOWNLOADING NEWEST PACKAGES...\n\n"
-echo "	GET dadp...\n\n"
-wget -N https://raw.githubusercontent.com/hauke96/LaTeX/master/dadp.sty
-echo "\n	GET gail...\n\n"
-wget -N https://raw.githubusercontent.com/hauke96/LaTeX/master/gail.sty
-echo "\n\n	DONE\n	(does not mean that everything just worked fine :P )\n"
-echo "	You can also use \"git clone https://github.com/hauke96/LaTeX.git\" to get get everything.\n\n"
+echo "DOWNLOADING NEWEST PACKAGES...\n"
+echo -n "GET dadp..."
+wget -N -q https://raw.githubusercontent.com/hauke96/LaTeX/master/dadp.sty
+echo "finished"
+echo -n "GET gail..."
+wget -N -q https://raw.githubusercontent.com/hauke96/LaTeX/master/gail.sty
+echo "finished"
+echo "\nDONE\n(does not mean that everything just worked fine :P )\n"
+echo "You can also use \"git clone https://github.com/hauke96/LaTeX.git\" to get get everything.\n\n"
