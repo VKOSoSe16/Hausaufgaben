f0b46bfd56cd2f323eb0666222d71059b5bfed89
hauke96 <mail@hauke-stieler.de>
2016-05-02 09:10:48 +0200

    Die beiden packages dadp und gail aktualisiert und das get-skript auch.

--- getpackages.sh
+++ getpackages.sh
@@ -1,9 +1,13 @@
-echo "DOWNLOADING NEWEST PACKAGES...\n"
+echo "DOWNLOADING NEWEST PACKAGES..."
+echo ""
 echo -n "GET dadp..."
 wget -N -q https://raw.githubusercontent.com/hauke96/LaTeX/master/dadp.sty
 echo "finished"
 echo -n "GET gail..."
 wget -N -q https://raw.githubusercontent.com/hauke96/LaTeX/master/gail.sty
 echo "finished"
-echo "\nDONE\n(does not mean that everything just worked fine :P )\n"
+echo ""
+echo "DONE"
+echo "(does not mean that everything just worked fine :P )"
+echo ""
 echo "You can also use \"git clone https://github.com/hauke96/LaTeX.git\" to get get everything.\n\n"
