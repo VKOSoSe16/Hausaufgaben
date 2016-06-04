1ccef1f5b848ec7e1da44ea138900d2bded44dea
Hauke Stieler <mail@hauke-stieler.de>
2016-06-04 19:15:45 +0200

    Aufgabe 7.1 angefangen und installationsscript geändert, sodass packages nun wirklich im system installiert werden und nicht nur in ./ kopiert werden.

--- getpackages.sh
+++ getpackages.sh
@@ -1,13 +1,43 @@
+#!/bin/bash
+path=/usr/share/texlive/texmf-dist/tex/generic/hauke96/
+whoami="$(whoami)"
+locally=$1
+
+if [ "$locally" = "locally" ]; then
+    path=./
+else
+    if [ "$whoami" != "root" ]; then
+        echo "This script must be executed as root!"
+        exit 1
+    fi
+fi
+
+download(){
+    if [ "$locally" = "locally" ]; then
+        wget -N -q $1 -P $path
+    else
+        sudo wget -N -q $1 -P $path
+    fi
+}
+
 echo "DOWNLOADING NEWEST PACKAGES..."
 echo ""
+if [ "$locally" != "locally" ]; then
+    echo -n "CREATE $path..."
+    sudo mkdir -p $path
+    echo "finished"
+fi
 echo -n "GET dadp..."
-wget -N -q https://raw.githubusercontent.com/hauke96/LaTeX/master/dadp.sty
+download https://raw.githubusercontent.com/hauke96/LaTeX/master/dadp.sty
 echo "finished"
 echo -n "GET gail..."
-wget -N -q https://raw.githubusercontent.com/hauke96/LaTeX/master/gail.sty
+download https://raw.githubusercontent.com/hauke96/LaTeX/master/gail.sty
 echo "finished"
 echo -n "GET makrocol..."
-wget -N -q https://raw.githubusercontent.com/hauke96/LaTeX/master/makrocol.sty
+download https://raw.githubusercontent.com/hauke96/LaTeX/master/makrocol.sty
+echo "finished"
+echo -n "UPDATE the lr-R file..."
+sudo texhash --quiet
 echo "finished"
 echo ""
 echo "DONE"
