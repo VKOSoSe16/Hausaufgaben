4655160560b336bd416ca621194bd560a37d1b17
Hauke Stieler <mail@hauke-stieler.de>
2016-06-19 20:09:31 +0200

    Das script fragt nun selbstständig nach root-Rechten. Habe außerdem den Beweis von Aufg. 1 gemacht. Bitte um review.

--- getpackages.sh
+++ getpackages.sh
@@ -6,9 +6,9 @@ locally=$1
 if [ "$locally" = "locally" ]; then
     path=./
 else
-    if [ "$whoami" != "root" ]; then
-        echo "This script must be executed as root!"
-        exit 1
+    if [ "$(id -u)" != "0" ]; then
+	sudo sh "./$0" "$@"
+	exit 0
     fi
 fi
 
@@ -36,11 +36,13 @@ echo "finished"
 echo -n "GET makrocol..."
 download https://raw.githubusercontent.com/hauke96/LaTeX/master/makrocol.sty
 echo "finished"
-echo -n "UPDATE the lr-R file..."
-sudo texhash --quiet
-echo "finished"
+if [ "$locally" != "locally" ]; then
+    echo -n "UPDATE the lr-R file..."
+    sudo texhash --quiet
+    echo "finished"
+fi
 echo ""
 echo "DONE"
 echo "(does not mean that everything just worked fine :P )"
 echo ""
-echo "You can also use \"git clone https://github.com/hauke96/LaTeX.git\" to get get everything.\n\n"
+echo "You can also use \"git clone https://github.com/hauke96/LaTeX.git\" to get get everything."
